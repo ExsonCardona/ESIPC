4a5b80536780db79ba21066db50a75237a1b9ff0
Exson Cardona <exsoncardona@hotmail.com>
2016-09-05 23:11:50 -0600

    Base para iniciar a desarrollar
    Si tienen dudas por ibox jajaja

---
+++ ESI/src/Conexion/conectar.java
@@ -0,0 +1,20 @@
+package Conexion;
+
+import java.sql.*;
+import javax.swing.*;
+
+
+public class conectar {
+Connection conect = null;
+   public Connection conexion()
+    {
+      try {
+             
+           
+           Class.forName("com.mysql.jdbc.Driver");
+           conect = DriverManager.getConnection("jdbc:mysql://localhost/bderickson","root","");
+           } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Error "+e);
+        }
+        return conect;
+     }}
